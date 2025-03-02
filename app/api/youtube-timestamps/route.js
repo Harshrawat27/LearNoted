777b03d1bee5f0af6dc1345b3e4c257fa7a7aa50
Harsh Rawat <harshrawat.dev@gmail.com>
2025-03-02 13:14:28 +0530

    fixed errors to depoly

--- app/api/youtube-timestamps/route.js
+++ app/api/youtube-timestamps/route.js
@@ -1,4 +1,4 @@
-import { NextResponse, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../../lib/authOptions';
 import dbConnect from '../../lib/dbConnect';
