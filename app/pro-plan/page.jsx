5f03c72bc2e943727da07f2a6bbf1588d51e04f3
Harsh Rawat <harshrawat.dev@gmail.com>
2025-03-12 23:41:43 +0530

    remove paypal ID from frontend that dropdown

--- app/pro-plan/page.jsx
+++ app/pro-plan/page.jsx
@@ -346,7 +346,7 @@ export default function ProPlanPage() {
                 <div className='bg-red-100 dark:bg-red-900/20 border border-red-200 dark:border-red-800 text-red-800 dark:text-red-300 px-4 py-4 rounded-xl mb-6'>
                   <div className='flex items-start'>
                     <AlertCircle className='h-5 w-5 mr-2 mt-0.5 flex-shrink-0' />
-                    <div>
+                    {/* <div>
                       <p className='font-semibold'>Error:</p>
                       <p>{paymentError}</p>
                       {debugInfo && (
@@ -359,7 +359,7 @@ export default function ProPlanPage() {
                           </pre>
                         </details>
                       )}
-                    </div>
+                    </div> */}
                   </div>
                 </div>
               )}
