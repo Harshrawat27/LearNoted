2e4f03ca1b59156649d5dcef953a35369634e19f
Harsh Rawat <harshrawat.dev@gmail.com>
2025-03-18 23:39:55 +0530

    error fixing

--- app/api/highlights/route.js
+++ app/api/highlights/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../../lib/authOptions';
 import dbConnect from '../../lib/dbConnect';
-import DomainHighlight from '../../../models/DomainHighlight';
+import DomainHighlight from '../../../models/Highlight';
 import jwt from 'jsonwebtoken';
 
 const corsHeaders = {
